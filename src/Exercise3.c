05e4529fba5d751d2d940c08f01c8fc42c856f74
TruongTanPhat <ITITIU20140@student.hcmiu.edu.vn>
2021-10-25 21:14:34 +0700

    exe 3 update

--- src/Exercise3.c
+++ src/Exercise3.c
@@ -63,13 +63,11 @@ void Ex3(int in_arr[], int n){
 	}
 				// part 2
 	for(int col =0; col < n; col++){		
-		int q = col;
-		for(int j = n - col -1; j > 0; j--, q++){
-			if(a[j][q] < a[j-1][q+1])
-			{
-				int temp=a[j][q];
-				a[j][q]=a[j-1][q+1];
-				a[j-1][q+1]=temp;  
+		for(int j = col -1; j >= 0; j--){
+			if (a[col][n - col - 1] < a[j][n - j - 1]) {
+				int temp = a[col][n - col - 1];
+				a[col][n - col - 1] = a[j][n - j - 1];
+				a[j][n - j - 1] = temp;
 			}
         }
             
